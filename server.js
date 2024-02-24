c2375463b5694980d1617e97070659bc7eea46ab
Kruddpippin <precious.op2013@gmail.com>
2024-02-24 14:47:18 +0100

    changed the query string functions; created a new function 'replaceHtml and attached the output instructions to it.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
-const http = require('http');
+const readline = require('readline');
+const fs = require('fs'); //this module enables us read files i.e 'file system'
+const http = require('http'); //http module
+const url = require('url'); //This url is to control query strings for the page
+
 
 
 // /*Read files using node js */
@@ -46,33 +49,47 @@ const html = fs.readFileSync('./index.html', 'utf-8')
 let posts = JSON.parse(fs.readFileSync('./posts.json', 'utf-8'));
 let postCatalogueHtml = fs.readFileSync('./post-catalogue.html', 'utf-8');
 
-let postsHtmlArray = posts.map((thePost) =>{
-    let output = postCatalogueHtml.replace('{Post_Placeholder}', thePost.dailyPost);
-        output = output.replace('{Image_Placeholder}', thePost.dailyImage);
+//This postHtmlArray is the variable that contains the mapping for the contents of the posts.json objects
+
+
+function replaceHtml(template, post){
+    let output = postCatalogueHtml.replace('{Post_Placeholder}', thePost.title); //shows the title of the post
+        output = output.replace('{Image_Placeholder}', thePost.dailyImage); //shows the image
+        output = output.replace('{ID}', thePost.id); //shows the id
 
         return output;
-} )
-
+}
 // Create a server
 // Step 1: Create
 
+//This handles everything that has to do with URL 
 const server = http.createServer((request, response) => {
-    let path = request.url;
+    //adding query names for resources
+    let {query, pathname: path} = url.parse(request.url, true)
+    // console.log(x);
+    // let path = request.url;
 
+    
+    
+    //  Routing for nav bar
     if (path === '/' || path.toLocaleLowerCase() === '/home') {
         response.writeHead(200, {
             'content-type': 'text/html',
             'krudd-header': 'Nice one Chief'
         });
-        response.end(html.replace('{Placeholder}', 'You are in Home Page'));
-
-    } 
+        response.end(html.replace('{Placeholder}', 'You are in Home Page'));} 
     
     else if (path.toLocaleLowerCase() === '/posts') {
-        let postsResponseHtml = html.replace('{Placeholder}', postsHtmlArray.join(','));
+        
+        if(!query.id){ //this checks for a query string before executing the next line.
+            let postHtmlArray =  posts.map((thePost) => {
+              replaceHtml(postCatalogueHtml, thePost);
+            })
+            let postsResponseHtml = html.replace('{Placeholder}', postsHtmlArray.join(','));
         response.writeHead(200, {'content-type': 'text/html'});
-        response.end(postsResponseHtml);
-    } 
+        response.end(postsResponseHtml);}
+        else{response.end('You are viewing post with ID ' + query.id)}
+    }
     
 
     else if (path === '/The_Man') {
@@ -80,24 +97,22 @@ const server = http.createServer((request, response) => {
             'content-type': 'text/html',
             'krudd-header': 'Nice one Chief'
         });
-        response.end(html.replace('{Placeholder}', 'You are in Profiles Page'))
-    } 
+        response.end(html.replace('{Placeholder}', 'You are in Profiles Page'))}
+
     
     else if (path.toLocaleLowerCase() === '/contact') {
         response.writeHead(200, {
             'content-type': 'text/html',
-            'krudd-header': 'Nice one Chief'
-        });
-        response.end(html.replace('{Placeholder}', 'You are in Contact Page'))
-    }
+            'krudd-header': 'Nice one Chief'});
+
+        response.end(html.replace('{Placeholder}', 'You are in Contact Page'))}
 
     else {
         response.writeHead(404, {
             'content-type': 'text/html',
-            'krudd-header': 'Nice one Chief'
-        });
-        response.end('Error 404: Not Found');
-    }
+            'krudd-header': 'Nice one Chief'});
+
+        response.end('Error 404: Not Found');}
 });
 
 
